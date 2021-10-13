8e25a6ec250eecc9f468e1656c690e0ef1713745
yufan <yufan@udel.edu>
2021-10-12 19:32:26 -0700

    Code Update test merge in main

--- src/App.tsx
+++ src/App.tsx
@@ -20,3 +20,31 @@ function App(): JSX.Element {
 export default App;
 
 //update test...
+//const APP_KEY = 'appWithRedux'
+
+//const App = () => {
+  //const appState = localStorage.getItem(APP_KEY)
+  //const initialState = appState ? JSON.parse(appState) : {
+    //events: [],
+    //operationLogs: []
+  //}
+  //const [state, dispatch] = useReducer(reducer, initialState)
+
+  //useEffect(() => {
+    //localStorage.setItem(APP_KEY, JSON.stringify(state))
+ // }, [state])
+
+ // return (
+   // <AppContext.Provider value={{ state, dispatch }}>
+  //    <div className="container-fluid">
+  //      <EventForm />
+  //      <Events />
+   //     <OperationLogs />
+   //   </div>
+   // </AppContext.Provider>
+ // )
+//}
+
+//export default App//
+
+//Code update Test
